15d6740eba94e499c03a23d1782a9cbd87f715e0
maxgolo89 <maxgolo89@gmail.com>
2018-01-20 14:44:51 +0200

    GPS implementation

--- battleShipApp/src/main/java/com/example/vladi/mybattleship/FinishGameActivity.java
+++ battleShipApp/src/main/java/com/example/vladi/mybattleship/FinishGameActivity.java
@@ -1,11 +1,15 @@
 package com.example.vladi.mybattleship;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,7 +22,10 @@ import android.widget.Toast;
 
 import com.example.vladi.mybattleship.DAL.RecordsDatabase;
 import com.example.vladi.mybattleship.Logic.Record;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class FinishGameActivity extends AppCompatActivity {
     private static final String TAG = "FinishGameActivity";
@@ -28,9 +35,14 @@ public class FinishGameActivity extends AppCompatActivity {
     private Button homeBtn;
     private Button submitScore;
     private EditText nameInput;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location loc = null;
+    private int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 999;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         setContentView(R.layout.activity_finish_game);
         getScore();
         setImage();
@@ -38,63 +50,63 @@ public class FinishGameActivity extends AppCompatActivity {
         setHomeBtn();
         setSubmitBox();
     }
-    private void setReplayBtn(){
-        replayBtn = (Button)findViewById(R.id.replayBtn);
+
+    private void setReplayBtn() {
+        replayBtn = (Button) findViewById(R.id.replayBtn);
         replayBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(FinishGameActivity.this,BattleActivity.class);
+                Intent intent = new Intent(FinishGameActivity.this, BattleActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
 
     }
-    private void setHomeBtn(){
-        homeBtn = (Button)findViewById(R.id.homeBtn);
+
+    private void setHomeBtn() {
+        homeBtn = (Button) findViewById(R.id.homeBtn);
         homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(FinishGameActivity.this,MainActivity.class);
+                Intent intent = new Intent(FinishGameActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
     }
-    private void setSubmitBox(){
+
+    private void setSubmitBox() {
         //set submit box visability
-        LinearLayout recordBox = (LinearLayout)findViewById(R.id.submitRecordBox);
-        if (score!=0)
+        LinearLayout recordBox = (LinearLayout) findViewById(R.id.submitRecordBox);
+        if (score != 0)
             recordBox.setVisibility(LinearLayout.VISIBLE);
         else
             recordBox.setVisibility(LinearLayout.GONE);
 
+        submitScore = (Button) findViewById(R.id.submitScoreBtn);
+        nameInput = (EditText) findViewById(R.id.nameInput);
 
-        submitScore = (Button)findViewById(R.id.submitScoreBtn);
-        nameInput = (EditText)findViewById(R.id.nameInput);
         //get box input
         submitScore.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 try {
-                    RecordsDatabase db = RecordsDatabase.getInstance(getApplicationContext());
-                    Location loc = getLastBestLocation();
-                    double lat = -33.868820;
-                    double lng = 151.209296;
-                    if(loc != null) {
-                        lat = loc.getLatitude();
-                        lng = loc.getLongitude();
+
+                    if (ActivityCompat.checkSelfPermission(FinishGameActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                            && ActivityCompat.checkSelfPermission(FinishGameActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                        // Request permission
+                        ActivityCompat.requestPermissions(FinishGameActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+                        Log.d("LOCATION", "Request location permission");
+                    } else {
+                        Log.d("LOCATION", "Permission exist");
+                        getLocationAndSaveToDatabase();
                     }
-                    db.recordsDao().createRecord(new Record(nameInput.getText().toString(), score, lat, lng));
-                    nameInput.setEnabled(false);
-                    submitScore.setEnabled(false);
-                    Toast toastSavedSuccess = Toast.makeText(getApplicationContext(), "Record Saved!", Toast.LENGTH_LONG);
-                    toastSavedSuccess.show();
-                } catch(Exception e) {
-                    Log.d("DB", e.toString());
-                    Toast toastFailedSave = Toast.makeText(getApplicationContext(), "Save failed!", Toast.LENGTH_LONG);
-                    toastFailedSave.show();
-                }
+
+
+                } catch(Exception e) {}
             }
         });
     }
@@ -113,31 +125,45 @@ public class FinishGameActivity extends AppCompatActivity {
     }
 
     @SuppressLint("MissingPermission")
-    private Location getLastBestLocation() {
-        LocationManager mLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        Location locationGPS;
-        Location locationNet;
-        try
-        {
-            locationGPS = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            locationNet = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        long GPSLocationTime = 0;
-        if (null != locationGPS) { GPSLocationTime = locationGPS.getTime(); }
-
-        long NetLocationTime = 0;
-
-        if (null != locationNet) {
-            NetLocationTime = locationNet.getTime();
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            Log.d("LOCATION", "Request Response");
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLocationAndSaveToDatabase();
+            }
         }
-
-        if ( 0 < GPSLocationTime - NetLocationTime ) {
-            return locationGPS;
-        }
-        else {
-            return locationNet;
-        }
-        } catch(Exception e) {Log.d("GPS", e.getMessage());}
-
-        return null;
     }
+    private void saveScoreToDatabase() {
+        try {
+            RecordsDatabase db = RecordsDatabase.getInstance(getApplicationContext());
+            double lat = -33.868820;
+            double lng = 151.209296;
+            if(loc != null) {
+                lat = loc.getLatitude();
+                lng = loc.getLongitude();
+            }
+            db.recordsDao().createRecord(new Record(nameInput.getText().toString(), score, lat, lng));
+            nameInput.setEnabled(false);
+            submitScore.setEnabled(false);
+            Toast toastSavedSuccess = Toast.makeText(getApplicationContext(), "Record Saved!", Toast.LENGTH_LONG);
+            toastSavedSuccess.show();
+        } catch(Exception e) {
+            Log.d("DB", e.toString());
+            Toast toastFailedSave = Toast.makeText(getApplicationContext(), "Save failed!", Toast.LENGTH_LONG);
+            toastFailedSave.show();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLocationAndSaveToDatabase() {
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(FinishGameActivity.this, (OnSuccessListener<Location>) location -> {
+            if (location != null) {
+                loc = location;
+            }
+            saveScoreToDatabase();
+        });
+    }
+
+
 }
